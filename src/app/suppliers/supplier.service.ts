e073ecfd0c05a8da609d8b59bc56d7cfcacf3118
Kerollos Fahmy <Kerollos.Fahmy@integrant.com>
2020-09-02 13:53:52 +0200

    higher order mapping

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, of, pipe } from 'rxjs';
+import { tap, concatMap, mergeMap, switchMap, delay } from 'rxjs/operators';
+import { Supplier } from './supplier';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,29 @@ import { throwError, Observable } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  suppliersWithConcatMap$ = of(1, 5, 8)
+    .pipe(
+      tap(item => console.log("concatMap source observable", item)),
+      concatMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+  suppliersWithMergeMap$ = of(1, 5, 8)
+    .pipe(
+      tap(item => console.log("mergeMap source observable", item)),
+      mergeMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+    suppliersWithSwitchMap$ = of(1, 5, 8)
+    .pipe(
+      tap(item => console.log("switchMap source observable", item)),
+      switchMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+  constructor(private http: HttpClient) {
+    this.suppliersWithConcatMap$.subscribe(item => console.log("Concat Map", item));
+    this.suppliersWithMergeMap$.subscribe(item => console.log("Merge Map", item));
+    this.suppliersWithSwitchMap$.subscribe(item => console.log("Switch Map", item));
+  }
 
   private handleError(err: any): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
